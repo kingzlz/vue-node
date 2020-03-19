ce45f965887125f4f6ae6b533b5e81d6adff2af6
zhanglizhu <kingzlz@163.com>
2020-03-19 14:18:21 +0800

    chore: 初始化项目

---
+++ admin/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import CategoryEdit from '../views/CategoryEdit.vue'
+import Main from '../views/Main.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'main',
+    component: Main,
+    children: [{
+      path: '/categories/create',
+      component: CategoryEdit
+    }]
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
